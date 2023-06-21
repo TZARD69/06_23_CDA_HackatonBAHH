3ef9caa2f3db79b077c59fd55a440bb1561e9e51
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-06-21 15:13:13 +0200

    Router Adrian

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/errorPage";
+import Routes from "./routes/routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Routes />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
