9b3c206acdb2d91e986e54a99bb254ed29af79bb
Brice <brice.verriere@epitech.eu>
2023-06-23 09:48:01 +0200

    modif front

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,10 +3,8 @@ import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -58,69 +56,83 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function Navbar() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <img src={logo} alt="logo entreprise externatic" />
-      <Typography variant="h3">Plus qu'un cabinet de recrutement</Typography>
-      <Box sx={{ width: "100%", maxWidth: 350 }}>
-        <AppBar
-          position="static"
+    <Box>
+      <Box sx={{ width: "100%" }}>
+        <img src={logo} alt="logo entreprise externatic" />
+        <Typography variant="h3">Plus qu'un cabinet de recrutement</Typography>
+        <Box
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-around",
-            backgroundColor: "lightGrey",
           }}
         >
-          <Toolbar sx={{ backgroundColor: "lightGrey", color: "black" }}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Job offers"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Box sx={{ width: "100%", maxWidth: 350 }}>
-        <AppBar
-          position="static"
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            backgroundColor: "lightGrey",
-          }}
-        >
-          <Toolbar sx={{ backgroundColor: "lightGrey", color: "black" }}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Where ?"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-          </Toolbar>
-        </AppBar>
-        <Button variant="contained" style={{ backgroundColor: "#CA2061" }}>
-          RECHERCHE
-        </Button>
-      </Box>
-      <Box>
-        <FormControlLabel required control={<Checkbox />} label="CDD" />
-        <FormControlLabel required control={<Checkbox />} label="CDI" />
-        <FormControlLabel required control={<Checkbox />} label="Internship" />
-        <FormControlLabel
-          required
-          control={<Checkbox />}
-          label="Apprenticeship"
-        />
-        <FormControlLabel required control={<Checkbox />} label="Part-time" />
-        <FormControlLabel required control={<Checkbox />} label="Full-time" />
+          <Box sx={{ width: "100%", maxWidth: 350, marginLeft: "20px" }}>
+            <AppBar
+              position="static"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                backgroundColor: "lightGrey",
+              }}
+            >
+              <Toolbar sx={{ backgroundColor: "lightGrey", color: "black" }}>
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Job offers"
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                </Search>
+              </Toolbar>
+            </AppBar>
+          </Box>
+          <Box sx={{ width: "100%", maxWidth: 350 }}>
+            <AppBar
+              position="static"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                backgroundColor: "lightGrey",
+              }}
+            >
+              <Toolbar sx={{ backgroundColor: "lightGrey", color: "black" }}>
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Location"
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                </Search>
+              </Toolbar>
+            </AppBar>
+          </Box>
+          <Button variant="contained" style={{ backgroundColor: "#CA2061" }}>
+            SEARCH
+          </Button>
+        </Box>
+        <Box>
+          <FormControlLabel required control={<Checkbox />} label="CDD" />
+          <FormControlLabel required control={<Checkbox />} label="CDI" />
+          <FormControlLabel
+            required
+            control={<Checkbox />}
+            label="Internship"
+          />
+          <FormControlLabel
+            required
+            control={<Checkbox />}
+            label="Apprenticeship"
+          />
+          <FormControlLabel required control={<Checkbox />} label="Part-time" />
+          <FormControlLabel required control={<Checkbox />} label="Full-time" />
+        </Box>
       </Box>
     </Box>
   );
