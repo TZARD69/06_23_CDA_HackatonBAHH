237f138c30b9d7bba39b1a9273781c8b91a2f57e
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-06-28 15:39:11 +0200

    Files checked. Database queries added in database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,26 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table smartphone
+(
+    id          int auto_increment
+        primary key,
+    model       varchar(25) not null,
+    os          varchar(10) not null,
+    ram_memory  int         not null,
+    storage     int         not null,
+    screen_size int         not null,
+    network     varchar(2)  not null,
+    charger     tinyint(1)  not null
+);
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+create table user
+(
+    id        int auto_increment
+        primary key,
+    firstname varchar(50)  not null,
+    lastname  varchar(50)  not null,
+    email     varchar(100) not null,
+    password  varchar(255) not null,
+    role      json         not null,
+    constraint user_pk2
+        unique (email)
+);
+
