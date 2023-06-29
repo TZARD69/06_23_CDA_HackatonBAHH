08e59fcc959d2b4bca5aa8f8e2a88cdc9d5c8e99
Hugo Delforce <delforceh@gmail.com>
2023-06-29 23:47:24 +0200

    modif page faq

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-
 import ButtonAppBar from "@components/Navbar";
 
-
-
-import Navbar from "@components/Navbar";
 import "./App.css";
 
 import { Outlet } from "react-router-dom";
 import Footer from "@components/Footer";
-
 
 function App() {
   return (
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,10 +3,10 @@ import { createTheme } from "@mui/material/styles";
 export const mainTheme = createTheme({
   palette: {
     primary: {
-      main: "#3E3E3E", 
+      main: "#3E3E3E",
     },
   },
-  
+
   typography: {
     fontFamily: ["Asap Condensed", "sans-serif"].join(","),
     fontSize: 13,
