b3555f1c33e8a844b2723dd723f882c262593ded
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-06-29 10:53:13 +0200

    Getting data from back to front working

--- frontend/src/pages/DataBase.jsx
+++ frontend/src/pages/DataBase.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card } from "@mui/material";
 
-function DataBase() {
+export default function DataBase() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7000/smartphones/")
+      .then((res) => setData(res.data));
+  }, []);
+
   return (
     <div>
-      <p> BDD</p>
+      <ul>
+        {data.map((smartphone) => (
+          <Card key={smartphone.id} title={smartphone.model}>
+            {smartphone.model}
+          </Card>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default DataBase;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { Typography, Button } from "@mui/material";
-import CardMedia from '@mui/material/CardMedia';
+import { Button, Typography } from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
 import { Box } from "@mui/system";
 
 export default function Home() {
   return (
     <Box sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
       <Typography variant="h3" sx={{ fontWeight: 500, p: "5px" }}>
-        Donnez une 2e vie solidaire à votre {" "}
-        <span style={{ color: '#E62460' }}>smartphone</span>
+        Donnez une 2e vie solidaire à votre{" "}
+        <span style={{ color: "#E62460" }}>smartphone</span>
       </Typography>
       <Typography variant="h4" sx={{ fontWeight: 400, p: "10px" }}>
         Notre formulaire en ligne vous permet de qualifier le matériel en
@@ -27,17 +27,17 @@ export default function Home() {
       </Box>
       <CardMedia
         component="img"
-        sx={{ width: 360, p: "20px"}}
+        sx={{ width: 360, p: "20px" }}
         image="./src/assets/homeimg.png"
         alt="home img"
       />
-      <Box sx={{ marginTop:"100px"}}>
+      <Box sx={{ marginTop: "100px" }}>
         <Typography variant="h3" sx={{ fontWeight: 500, p: "5px" }}>
-          Accède à notre {" "}
-          <span style={{ color: '#00ACB0' }}>base de donée</span>
+          Accède à notre <span style={{ color: "#00ACB0" }}>base de donée</span>
         </Typography>
         <Typography variant="h4" sx={{ fontWeight: 400, p: "10px" }}>
-          Accède à notre base de données de smartphones et découvre toutes les informations détaillées.
+          Accède à notre base de données de smartphones et découvre toutes les
+          informations détaillées.
         </Typography>
         <Box sx={{ p: "20px" }}>
           <Button
